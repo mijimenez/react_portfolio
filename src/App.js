edf5d0236a9bfc6829bd494ba2d1aec850d3523c
Madeline Jimenez <madelineijimenez@gmail.com>
2020-04-03 07:40:43 -0400

    Set up sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
